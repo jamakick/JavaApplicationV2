d1c4e5663423ea6977b8ce06be422bc66383ab83
alexgrov <alexgrov@IULB-IF0006-13.ads.iu.edu>
2018-03-08 14:01:56 -0500

    Earthquake Controller commit

--- Earthquake_Data_Team3/src/application/EarthquakeDataController.java
+++ Earthquake_Data_Team3/src/application/EarthquakeDataController.java
@@ -1,6 +1,8 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
 
 import javafx.scene.control.Button;
 
@@ -15,5 +17,30 @@ public class EarthquakeDataController {
 	private Button printby;
 	@FXML
 	private Button search;
+	
+	
+	@FXML
+	private void helpButtonClick (ActionEvent event) {
+		System.out.println("Help Button Clicked");
+	}
+	
+	@FXML
+	private void summaryButtonClick (ActionEvent event) {
+		System.out.println("Summary Button Clicked");
+	}
 
+	@FXML
+	private void printButtonClick (ActionEvent event) {
+		System.out.println("Print Button Clicked");
+	}
+	
+	@FXML
+	private void printByButtonClick (ActionEvent event) {
+		System.out.println("PrintBy Button Clicked");
+	}
+	
+	@FXML
+	private void searchButtonClick (ActionEvent event) {
+		System.out.println("Search Button Clicked");
+	}
 }
