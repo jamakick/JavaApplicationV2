5376f96300eb22729e9784fedf1abd7e688dc36c
alexgrov <alexgrov@IULB-IF0006-13.ads.iu.edu>
2018-03-08 13:56:30 -0500

    Gui and controller created

---
+++ Earthquake_Data_Team3/src/application/EarthquakeDataController.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+public class EarthquakeDataController {
+	@FXML
+	private Button help;
+	@FXML
+	private Button summary;
+	@FXML
+	private Button print;
+	@FXML
+	private Button printby;
+	@FXML
+	private Button search;
+
+}
