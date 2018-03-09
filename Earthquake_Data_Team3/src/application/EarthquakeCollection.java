252bbdd58418af53cf02736425a6266c7e7cdf3e
origlehe <origlehe@IULB-IF0006-14.ads.iu.edu>
2018-03-09 15:27:53 -0500

    Added EarthquakeCollection Class structure

---
+++ Earthquake_Data_Team3/src/application/EarthquakeCollection.java
@@ -0,0 +1,18 @@
+package application;
+import java.util.ArrayList;
+
+public class EarthquakeCollection {
+//Holds EarthQuake Objects
+	public static void main(String[] args) {
+		ArrayList<EarthQuake> earthQList = new ArrayList<>();
+	//Read in data from EarthQuake Class' CSV file
+		earthQList.add(new EarthQuake(/*Information from data read in*/));
+	
+		//@Override statement for 
+		//toString() method that summarizes EarthQuakeCollection into a string
+		
+		
+		//SearchBy methods to return earthQList
+	}
+	
+}
