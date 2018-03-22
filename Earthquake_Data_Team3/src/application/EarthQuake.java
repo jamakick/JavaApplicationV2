044c087fb30e49543513f58c7b65b86cc8b3e553
jackarno <jackarno@umail.iu.edu>
2018-03-22 13:28:36 -0400

    Fixed reading in the csv files

--- Earthquake_Data_Team3/src/application/EarthQuake.java
+++ Earthquake_Data_Team3/src/application/EarthQuake.java
@@ -1,13 +1,25 @@
 package application;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class EarthQuake {
 	//Create a file object
 	public static void main(String[] args) {
 		//Read in data from csv file
-		java.io.File file = new java.io.File("all_month.csv");
-		try ( Scanner input = new Scanner(file)) {
+		java.io.File theFile = new java.io.File("all_month.csv");
+		try (Scanner input = new Scanner(theFile)) {
 			//Read first line of headers
+			int i = 0;
+			while (input.hasNextLine()) {
+			String[] theThing = input.nextLine().split(",");
+			for (int j = 0; 22 > j; j++) {
+			System.out.print(theThing[j] + "\t");}
+			System.out.println();
+			}
 			
 		}
 		catch (Exception ex) {
--- Earthquake_Data_Team3/src/application/Main.java
+++ Earthquake_Data_Team3/src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -11,15 +13,25 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		System.out.print("Im so good at this. BOOM");
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// set a title for the Window
+			primaryStage.setTitle("Earthquake Window");
+			
+			// get an FXML loader and read in the fxml code
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("/EarthquakeData.fxml"));
+			AnchorPane mainLayout = (AnchorPane)loader.load();
+			
+			// Create the scene with the layout in the fxml code, set the scene and show it
+			Scene scene = new Scene(mainLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	
 	
 	public static void main(String[] args) {
 		launch(args);
