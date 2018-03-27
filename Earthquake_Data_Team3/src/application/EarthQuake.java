c7e00d184ea0ed5af622169d5564195089bcee6f
alexgrov <alexgrov@IULB-IF0006-13.ads.iu.edu>
2018-03-27 13:56:05 -0400

    Gui functionality

--- Earthquake_Data_Team3/src/application/EarthQuake.java
+++ Earthquake_Data_Team3/src/application/EarthQuake.java
@@ -31,23 +31,13 @@ public class EarthQuake  {
 		
 	}
 	
-	public class EarthQuake(Date time, double latitude, double longitude, double depth, 
+	public EarthQuake(Date time, double latitude, double longitude, double depth, 
 			double mag, String magType, int nst, double gap, double dMin, double rms, String net,
 			String id, Date updated, String place, String type, double horizontal, double depthError, 
 			double magError, double magNst, String status, String locationSource, String magSource) {
 		
 	}
 	
-	public static void main(String[] args) {
-		//Read in data from csv file
-		java.io.File file = new java.io.File("all_month.csv");
-		try ( Scanner input = new Scanner(file)) {
-			//Read first line of headers
-			
-		}
-		catch (Exception ex) {
-			System.out.println("File could not be opened or data did not match.");
-		}
-	}
+	
 
 }
