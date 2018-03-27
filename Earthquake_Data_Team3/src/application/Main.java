53127ab77f4fef0e76ac5f741b06f87e5a862627
alexgrov <alexgrov@IULB-IF0006-13.ads.iu.edu>
2018-03-27 13:57:00 -0400

    GUI

--- Earthquake_Data_Team3/src/application/Main.java
+++ Earthquake_Data_Team3/src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -11,9 +13,15 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		System.out.print("Im so good at this. BOOM");
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+primaryStage.setTitle("Earthquake Window");
+			
+			// get an FXML loader and read in the fxml code
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("/EarthquakeData.fxml"));
+			AnchorPane mainLayout = (AnchorPane)loader.load();
+			
+			// Create the scene with the layout in the fxml code, set the scene and show it
+			Scene scene = new Scene(mainLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
