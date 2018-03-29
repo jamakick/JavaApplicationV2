6074062b3c4ad40332de428f8ab8c1e6ba464739
alexgrov <alexgrov@IULB-IF0006-13.ads.iu.edu>
2018-03-29 14:07:58 -0400

    d

--- Earthquake_Data_Team3/src/application/EarthQuake.java
+++ Earthquake_Data_Team3/src/application/EarthQuake.java
@@ -1,50 +1,83 @@
 package application;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Date;
+import java.util.ArrayList;
 
 
+public class EarthQuake {
+	public static void main(String[] args) {
+		//Create a file object
+		/*
+		Date time;
+		String latitude;
+		String longitude;
+		String depth;
+		String mag;
+		String magType;
+		int nst;
+		String gap;
+		String dMin;
+		String rms;
+		String net;
+		String id;
+		Date updated;
+		String place;
+		String type;
+		String horizontal;
+		String depthError;
+		String magError;
+		String magNst;
+		String status;
+		String locationSource;
+		String magSource;
+		*/
+		String time;
+		String latitude;
+		String longitude;
+		String depth;
+		String mag;
+		String magType;
+		String nst;
+		String gap;
+		String dMin;
+		String rms;
+		String net;
+		String id;
+		String updated;
+		String place;
+		String type;
+		String horizontal;
+		String depthError;
+		String magError;
+		String magNst;
+		String status;
+		String locationSource;
+		String magSource;
+		
 
-public class EarthQuake  {
-	//Create a file object
-	Date time;
-	double latitude;
-	double longitude;
-	double depth;
-	double mag;
-	String magType;
-	int nst;
-	double gap;
-	double dMin;
-	double rms;
-	String net;
-	String id;
-	Date updated;
-	String place;
-	String type;
-	double horizontal;
-	double depthError;
-	double magError;
-	double magNst;
-	String status;
-	String locationSource;
-	String magSource;
 	
-
-
-
 	
-	
-	public EarthQuake() {
+}
+	// No arg-constructor. This means NULL will be used for each value so far
+	public EarthQuake( ) {
 		
 	}
-	
-	public EarthQuake(Date time, double latitude, double longitude, double depth, 
-			double mag, String magType, int nst, double gap, double dMin, double rms, String net,
-			String id, Date updated, String place, String type, double horizontal, double depthError, 
-			double magError, double magNst, String status, String locationSource, String magSource) {
 		
-	}
+
 	
+	public EarthQuake(String time, String latitude, String longitude, String depth, 
+			String mag, String magType, String nst, String gap, String dMin, String rms, String net,
+			String id, String updated, String place, String type, String horizontal, String depthError, 
+			String magError, String magNst, String status, String locationSource, String magSource) {
+		
+
+			
+	}
+
 
 	
 	
