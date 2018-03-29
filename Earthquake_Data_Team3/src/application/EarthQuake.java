92df18f351f4de68f6cfa8e1daba2bdab92dea1e
alexgrov <alexgrov@IULB-IF0006-13.ads.iu.edu>
2018-03-29 13:25:33 -0400

    cli

--- Earthquake_Data_Team3/src/application/EarthQuake.java
+++ Earthquake_Data_Team3/src/application/EarthQuake.java
@@ -2,6 +2,8 @@ package application;
 import java.util.Scanner;
 import java.sql.Date;
 
+
+
 public class EarthQuake  {
 	//Create a file object
 	Date time;
@@ -27,6 +29,11 @@ public class EarthQuake  {
 	String locationSource;
 	String magSource;
 	
+
+
+
+	
+	
 	public EarthQuake() {
 		
 	}
@@ -38,13 +45,29 @@ public class EarthQuake  {
 		
 	}
 	
+
+	
+	
+	
+
+
+
+public static void commandLine (String[] args) {
+	
 	Scanner input = new Scanner(System.in);
-	
-	switch (input) {
-	case 
+	System.out.print("Please enter a command: ");
+	String command = input.next();
+	switch (command) {
+	case "help": System.out.println("Sumarry Button: print out a summary of all of the data (# of events, timerange of the events,etc) Click on button to invoke." + "/n" + 
+			"Print Button: Prints out all the earthquake events. Click on button to invoke." + "/n" + "Print By Button: Print out all the Earthquake events, sorted by some field (date, depth, mag, place, status) Click on button, then set field to sort by."
+			+ "/n" + "Search Button: Print out all of the earthquake events that meet some criteria (date, location, depth, mag, magType, place, status). Click on button, then set field to search." 
+			+ "/n" + "Help Button: Prints out the description of buttons and how to invoke them. Click on button to invoke.");
+	case "summary": ;
+	case "print": ;
+	case "printby": ;
+	case "search": ; 
+				
 	}
-	
-	
+	}
 
 }
-
