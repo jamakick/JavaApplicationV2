6ff82e2cbe46efcd08a45af97d40d8ea468ab31b
origlehe <origlehe@IULB-IF0006-14.ads.iu.edu>
2018-03-30 15:42:15 -0400

    Override to string almost done.

--- Earthquake_Data_Team3/src/application/EarthQuake.java
+++ Earthquake_Data_Team3/src/application/EarthQuake.java
@@ -68,14 +68,19 @@ public class EarthQuake {
 	}
 		
 
-	
-	public EarthQuake(String time, String latitude, String longitude, String depth, 
-			String mag, String magType, String nst, String gap, String dMin, String rms, String net,
-			String id, String updated, String place, String type, String horizontal, String depthError, 
+	public EarthQuake(String /*time*/, String /*latitude*/, String /*longitude*/, String /*depth*/, 
+			String /*mag*/, String /*magType*/, String /*nst*/, String /*gap*/, String /*dMin*/, String rms, String net,
+			String /*id*/, String updated, String /*place*/, String type, String horizontal, String depthError, 
 			String magError, String magNst, String status, String locationSource, String magSource) {
 		
 
 			
+	}
+	@Override
+	public Override toString() {
+		return "Earthquake Location: " + locationSource +"\n" + "Preferred data source: "+net+"\n" + "EarthQuake ID:" + id + "\n" + "Magnitude: "+ mag + "\n" + "Magnitude Type: " + magType + "\n" + "Latitude: " + latitude + "\n" + "Longitude: " + longitude +
+				+ "\n" + "Depth: " + depth + "\n" + "Seismic Stations required: "+ nst + "\n" + "Largest azimuthal gap between stations: "+ gap + "\n" + "Distance to nearest Station: "+ dmin +"\n" + "Location nearest Event: "+
+				place + "\n" + "Type of Seismic event: "+ type + "\n"+ "Review Status: " + status +"\n" + ""
 	}
 }
 
