12ff886b2d22b03881a33eb0401b47805de1e111
davisadj <davisadj@IULB-IF0006-20.ads.iu.edu>
2018-04-26 14:11:32 -0400

    Functioning markers and info windows, need on click method

--- Earthquake_Data_Team3/src/application/MainController.java
+++ Earthquake_Data_Team3/src/application/MainController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import netscape.javascript.JSObject;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -11,14 +12,16 @@ import java.util.ResourceBundle;
 
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.InfoWindow;
+import com.lynden.gmapsfx.javascript.object.InfoWindowOptions;
 import com.lynden.gmapsfx.javascript.object.LatLong;
 import com.lynden.gmapsfx.javascript.object.MapOptions;
 import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
 import com.lynden.gmapsfx.javascript.object.Marker;
 import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 import javafx.event.ActionEvent;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.ChoiceBox;
 
 
@@ -26,15 +29,9 @@ public class MainController implements Initializable, MapComponentInitializedLis
 	@FXML
 	private Button help;
 	@FXML
-	private Button dataSummary;
+	private Button search;
 	@FXML
-	private TextArea output;
-	@FXML
-	private Button allEvents;
-	@FXML
-	private Button searchBy;
-	@FXML
-	private Button printBy;
+	private Button reset;
 	@FXML
 	private Button selectionOut;
 	@FXML
@@ -42,9 +39,9 @@ public class MainController implements Initializable, MapComponentInitializedLis
 	@FXML
 	private ChoiceBox<String> searchDrop;
 	@FXML
-	private TextField searchText;
+	private TextField min;
 	@FXML
-	private TextField searchText2;
+	private TextField max;
 	@FXML
 	private GoogleMapView mapView;
 	
@@ -55,54 +52,40 @@ public class MainController implements Initializable, MapComponentInitializedLis
 	// Event Listener on Button[#help].onAction
 	@FXML
 	public void helpAction(ActionEvent event) {
+		@SuppressWarnings("unused")
 		String outstring= "Summary Button: print out a summary of all of the data (# of events, timerange of the events,etc) Type summary to invoke." + "\n \n" + 
 				"Print Button: Prints out all the earthquake events. Type print to invoke." + "\n \n" + "Print By Button: Print out all the Earthquake events, sorted by some field (date, depth, mag, place, status) Click on button, then set field to sort by."
 				+ "\n \n" + "Search Button: Print out all of the earthquake events that meet some criteria (date, location, depth, magnitude, magType, place, status). Type search, then set field to search." 
 				+ "\n \n" + "Help Button: Prints out the description of buttons and how to invoke them. Type help to invoke."+ "\n \n" + "Type 'quit' to break the command line.";
-		output.setText(outstring);
 	}
-	// Event Listener on Button[#dataSummary].onAction
-	@FXML
-	public void dataSummaryAction(ActionEvent event) {
-		output.setText("Data summary...");
-	}
-	// Event Listener on Button[#allEvents].onAction
-	@FXML
-	public void allEventsAction(ActionEvent event) {
-		output.setText("All events...");
-	}
+
 	
 	@FXML
 	public void allOutAction(ActionEvent event) {
-        output.setText("Outputing all to text...");
 	}
 	
 	@FXML
 	public void selectionOutAction(ActionEvent event) {
-        output.setText("Outputing selection to text...");
 	}
 	
 	// Event Listener on Button[#Search].onAction
 	@FXML
-	public void searchByAction(ActionEvent event) {
-		 output.setText("search by...");
+	public void searchAction(ActionEvent event) {
 	}
 	
 	@FXML
-	public void printByAction(ActionEvent event) {
-		 output.setText("print by...");
+	public void resetAction(ActionEvent event) {
 	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		output.setEditable(false);
 		mapView.addMapInializedListener(this);
 			
 	}
 	
 	@Override
 	public void mapInitialized() {
-		LatLong loc1 = new LatLong(39.1710061, -86.51679969999998);
+		//LatLong loc1 = new LatLong(39.1710061, -86.51679969999998);
 		
 		MapOptions mapOptions = new MapOptions();
 		
@@ -111,12 +94,39 @@ public class MainController implements Initializable, MapComponentInitializedLis
 		
 		map = mapView.createMap(mapOptions, false);
 		
-		MarkerOptions markerOptions1 = new MarkerOptions();
-        markerOptions1.position(loc1);
-        
-        Marker loc1Marker = new Marker(markerOptions1);
-  
-		map.addMarker(loc1Marker);
+		ArrayList<EarthQuake> earthquakes = Console.getFileInformation();
+		
+		for(int i=0;i<50;i++) {
+			double latitude = Double.parseDouble(earthquakes.get(i).getLat());
+			double longitude = Double.parseDouble(earthquakes.get(i).getLong());
+			
+			LatLong quakeLoc= new LatLong(latitude, longitude);
+			
+			MarkerOptions quakeOptions = new MarkerOptions();
+			quakeOptions.position(quakeLoc);
+			
+			Marker quakeMarker = new Marker(quakeOptions);
+			
+			map.addMarker(quakeMarker);
+			
+			InfoWindowOptions quakeWindowOptions = new InfoWindowOptions();
+	        quakeWindowOptions.content(earthquakes.get(i).toString());
+
+	        InfoWindow quakeWindow = new InfoWindow(quakeWindowOptions);
+	        
+	        map.addUIEventHandler(quakeMarker, UIEventType.click, (JSObject obj) -> {
+	            quakeWindow.open(map);
+	        });
+	        
+			
+		}
+		
+//		MarkerOptions markerOptions1 = new MarkerOptions();
+//        markerOptions1.position(loc1);
+//        
+//        Marker loc1Marker = new Marker(markerOptions1);
+//  
+//		map.addMarker(loc1Marker);
         
 	}
 }
